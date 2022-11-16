341d0e19c0fa622ef585097bd6daf42d671a7d10
rudtn4038 <mm453@naver.com>
2022-11-16 22:00:00 +0900

    챕터1 변수상수

---
+++ src/main/java/j01_변수상수/Constant.java
@@ -0,0 +1,14 @@
+package j01_변수상수;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		
+		final double PI = 3.14;
+		
+		final String FILE_PATH = "C:/test/java.jpg";
+
+		System.out.println(PI);
+	}
+
+}
